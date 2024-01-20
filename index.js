fe0c11e57c306b05ee0089f1b3454aebfb00e267
Anwesha Sarkar <anweshasarkar29@gmail.com>
2024-01-20 12:21:01 +0530

    chore: added version in response

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ app.use(bodyParser.json());
 app.get("/", (req, res, next) => {
   res
     .status(200)
-    .json({ status: "success", message: "server is running properly" });
+    .json({
+      status: "success",
+      message: "server is running properly",
+      version: "1.1",
+    });
 });
 app.listen(3000, () => {
   console.log(`server is listneing on http://localhost:3000`);
